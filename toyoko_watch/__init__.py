a25ac6013050acf7ef1c411d5f1452638e66349d
HUvz <91484306+huvz04@users.noreply.github.com>
2026-07-14 19:03:27 +0800

    feat: scaffold Toyoko watch plugin

---
+++ toyoko_watch/__init__.py
@@ -0,0 +1,5 @@
+"""Core services for the AstrBot Toyoko availability plugin."""
+
+from .models import NotificationTarget, RequirementSlot, Vacancy, WatchTask
+
+__all__ = ["NotificationTarget", "RequirementSlot", "Vacancy", "WatchTask"]
